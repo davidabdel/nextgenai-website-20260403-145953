c7eedc78b87866cb14ab146dc0d6d699d7d7b9c5
davidabdel <davidabdel@users.noreply.github.com>
2026-04-09 15:06:05 +1000

    feat: integrate PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import posthog from 'posthog-js';
 import App from './App.tsx';
 import './index.css';
+
+posthog.init('phc_tiHTiiGHu7JVsE2Woxr5h42UVJpyjXP2KChGYWjwuAhM', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'always',
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
